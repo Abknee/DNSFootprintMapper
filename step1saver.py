86adb7334e15b74be811b998785bec2ccbe690d4
Shawn Abney <46576633+Abknee@users.noreply.github.com>
2021-10-21 16:01:35 -0500

    Update step1saver.py

--- step1saver.py
+++ step1saver.py
@@ -5,9 +5,17 @@ inputList = sys.argv[1]
 with open(inputList, "r", encoding='utf-8-sig') as domainBucket:
     for line in domainBucket:
         targetDomain = line
-        url1 = "https://subdomains.whoisxmlapi.com/api/v1?apiKey=$APIKEY&domainName={0}".format(
+        url1 = "https://subdomains.whoisxmlapi.com/api/v1?apiKey=$APIKEY&domainName={}".format(
             targetDomain)
-        response = requests.get(url1).json()
-        for subs in response["result"]["records"]:
+        url2 = "https://api.threatintelligenceplatform.com/v1/sslConfiguration?apiKey=$APIKEY&domainName={}".format(
+            targetDomain)
+        responseUrl1 = requests.get(url1).json()
+        responseUrl2 = requests.get(url2).json()
+        data2 = responseUrl2['testResults']
+        data1 = responseUrl1['result']['records']
+        for x in data1:
             with open('output.csv', 'a') as f:
-                print((subs['domain']), file=f)
+                if data2["sslCertificateConfigured"] == True and data2["validFrom"]["status"] != "OK":
+                    print("the domain " + x['domain'] + " SSL check has: " + data2["validFrom"]["status"], file=f)
+                else:
+                    print('SSL is good for ' + x['domain'], file=f)
